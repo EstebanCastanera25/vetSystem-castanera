8ee51b92676aed52b1ac5361dad0200bc5a57088
Esteban <estebancastanera@gmail.com>
2026-09-22 19:54:32 -0300

    se agrego la entidad Medicamento con stock y precio unitario

---
+++ src/main/java/com/vetSystem/Entity/Medicamento.java
@@ -0,0 +1,32 @@
+
+package com.vetSystem.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+
+@Entity
+@Table(name = "medicamentos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Medicamento {
+
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(name = "precio_unitario", nullable = false, scale = 2)
+    private BigDecimal precioUnitario;
+    
+}
