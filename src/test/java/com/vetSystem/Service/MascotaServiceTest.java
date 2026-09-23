07ed7ca6e5383e116fb49bd16c072b90ba0c5c84
Esteban <estebancastanera@gmail.com>
2026-09-22 21:29:39 -0300

    se arreglaron los imports que impedian compilar los tests de MascotaService

--- src/test/java/com/vetSystem/Service/MascotaServiceTest.java
+++ src/test/java/com/vetSystem/Service/MascotaServiceTest.java
@@ -8,7 +8,7 @@ import com.vetSystem.Mapper.MascotaMapper;
 import com.vetSystem.Repository.DuenioRepository;
 import com.vetSystem.Repository.MascotaRepository;
 
-import main.java.com.vetSystem.Exception.CupoExcedidoException;
+import com.vetSystem.Exception.CupoExcedidoException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,6 +26,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.never;
